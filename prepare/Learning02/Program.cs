7b8cb1b1529502cb055ad08019af6565b5455742
MTristan <53493044+MTristan012@users.noreply.github.com>
2024-05-01 15:43:16 -0600

    W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Automatization Assistant";
+        job1._company = "Grupo Victor";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Ingeaa";
+        job2._startYear = 2023;
+        job2._endYear = 2030;
+
+        Resume myResume = new Resume();
+        myResume._name = "Tristan Perea";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
